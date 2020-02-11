1f9aad79f52958b9df2193b161413db1a9b903d8
sospmy <36981805+sospmy@users.noreply.github.com>
2020-02-11 18:41:50 +0800

    ex5

---
+++ ShipState.java
@@ -0,0 +1,39 @@
+
+public class ShipState {
+	public AbstractShip ship;
+	public boolean struck;
+	protected int count_strike;
+	
+	public void addStrike() {
+		struck=true;
+		count_strike++;
+	}
+	
+	public boolean isStruck() {
+		return struck;
+	}
+	
+	public String toString() {
+		if(struck)
+			System.out.print(ColorUtil.colorize(ship.label,ColorUtil.Color.RED));
+		else
+			System.out.println(ship.label);
+		return ship.name;
+	}
+	
+	public boolean isSunk() {
+		if(count_strike>=ship.taille)
+			return true;
+		else
+			return false;
+	}
+	
+	
+	public  AbstractShip getship() {
+		ship.name= toString();
+		if(isSunk())
+			System.out.println("It is sunk");
+		return ship;
+	}
+	
+}
